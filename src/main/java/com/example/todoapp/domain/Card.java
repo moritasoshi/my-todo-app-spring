5d229c1eda6cd47e1864c195d201f3418d024a86
moritasoshi <soshi.morita@rakus-partners.co.jp>
2020-09-17 15:59:03 +0900

    fix

--- src/main/java/com/example/todoapp/domain/Card.java
+++
@@ -1,9 +0,0 @@
-package com.example.todoapp.domain;
-
-import lombok.Data;
-
-@Data
-public class Card {
-    private String card_id;
-    private String name;
-}
--- src/main/java/com/example/todoapp/domain/User.java
+++
@@ -1,9 +0,0 @@
-package com.example.todoapp.domain;
-
-import lombok.Data;
-
-@Data
-public class User {
-    private String id;
-    private String uid;
-}
