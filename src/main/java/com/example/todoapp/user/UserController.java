5fc3240d391c612c7d533bf3f8b76cd3579c5b80
moritasoshi <soshi.morita@rakus-partners.co.jp>
2020-11-07 10:46:53 +0900

    Implement JWT Authentication #27

--- src/main/java/com/example/todoapp/user/UserController.java
+++ src/main/java/com/example/todoapp/user/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/user")
 public class UserController {
     @Autowired
     private ApplicationUserMapper applicationUserMapper;
